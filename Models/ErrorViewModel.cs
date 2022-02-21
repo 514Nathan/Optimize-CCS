9ff53989005e0d1c94ce1232d93acb4f8fd1b4bd
Nhat-N <514nhatnguyen@gmail.com>
2022-02-21 03:28:17 -0500

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Optimize_CCS.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
